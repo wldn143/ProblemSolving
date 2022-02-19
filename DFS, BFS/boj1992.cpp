fb5f46e75b3ee064ba9b20420aeb9db437ab45b5
jw <70766701+wldn143@users.noreply.github.com>
2022-02-19 23:40:13 +0900

    Create boj1992.cpp
    https://www.acmicpc.net/problem/1992
    쿼드트리

---
+++ DFS, BFS/boj1992.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int n, k;
+int a[65][65], x, y;
+string s;
+typedef long long int ll;
+
+string quard(int y, int x, int size) {
+	if (size == 1) return string(1, a[y][x]);
+	char b = a[y][x];
+	string ret = "";
+	bool flag = 0;
+	for (int i = y; i < y + size; i++) {
+		for (int j = x; j < x + size; j++) {
+			if (b != a[i][j]) {
+				ret += '(';
+				ret += quard(y, x, size / 2);
+				ret += quard(y, x + size / 2, size / 2);
+				ret += quard(y + size / 2, x, size / 2);
+				ret += quard(y + size / 2, x + size / 2, size / 2);
+				ret += ')';
+				return ret;
+
+			}
+		}
+	}
+	return string(1, a[y][x]);
+}
+
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < n; j++) {
+			a[i][j] = s[j];
+		}
+	}
+
+	cout << quard(0, 0, n) << "\n";
+}
