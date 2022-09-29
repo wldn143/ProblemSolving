1c105fb7a9bab04055f6fd8472a556aea843d914
wldn143 <wldn143@gmail.com>
2022-09-29 10:12:37 +0900

    백준 2910 빈도 정렬

--- 그래프이론, DFS, BFS/boj2910.cpp
+++ 그래프이론, DFS, BFS/boj2910.cpp
@@ -1,67 +1,51 @@
 #include <iostream>
+#include <algorithm>
+#include <stdio.h>
 #include <vector>
 using namespace std;
-vector<pair<int, int>> v;
+int n, c;
 vector<pair<int, int>> a;
-//int a[1004];
-int n, c, x,flag,idx,idx2;
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+	return a.second > b.second;
+}
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	cin >> n >> c;
-	for (int i = 0; i < n; i++) {
-		v.push_back(make_pair(0, 0));
-		a.push_back(make_pair(0, 0));
-	}
-	for (int i = 0; i < n; i++) {
-		cin >> x;
-		for (int j = 0; j < n; j++) {
-			if (a[j].first == x) { //탐색시 발견
-				flag = 1;
-				idx2 = j;
+	for (int i = 0; i < n; i++)
+	{
+		int k;
+		cin >> k;
+		//아 애초에 a.size가 0이구나...
+		if (a.size() == 0)
+		{
+			a.push_back(make_pair(k, 1));
+			continue;
+		}
+		for (int j = 0; j < a.size(); j++)
+		{
+			if (a[j].first == k)
+			{
+
+				a[j].second++;
 				break;
 			}
-		}
-
-		if (flag == 1) {//발견하면
-			v[idx2].second++;
-			flag = 0;
-		}
-		
-		else {
-			a[idx].first = x;
-			a[idx].second=idx;
-			v[idx].first = x;
-			v[idx].second++;
-			idx++;
-		}
-	}
-	
-	int k = 0;
-	int tmp1 = 0;
-	int tmp2 = 0;
-
-	while (1) {
-		if (k == n - 1)break;
-		if (v[k + 1].second > v[k].second) {
-			tmp1 = v[k].first;
-			tmp2 = v[k].second;
-			v[k].first = v[k + 1].first;
-			v[k].second = v[k + 1].second;
-			v[k + 1].first = tmp1;
-			v[k + 1].second = tmp2;
-			k = 0;
-		}
-		else {
-			k++;
-		}
-	}
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < v[i].second; j++) {
-			if (v[i].first > 0) {
-				cout << v[i].first << " ";
-
+			if (j == a.size() - 1)
+			{
+				a.push_back(make_pair(k, 0));
 			}
+		}
+	}
+	stable_sort(a.begin(), a.end(), compare);
+	for (int i = 0; i < a.size(); i++)
+	{
+		for (int j = 0; j < a[i].second; j++)
+		{
+			cout << a[i].first << " ";
 		}
 	}
 }
