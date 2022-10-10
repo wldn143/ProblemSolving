c7120f0c2900b45d58126021a1c338ec7eaba5a8
wldn143 <wldn143@gmail.com>
2022-10-10 13:23:58 +0900

    백준 12869 뮤탈리스크

---
+++ 완전탐색, 백트래킹/boj12869.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+int n;
+int a[3], visited[64][64][64];
+int _a[6][3] = {
+    {9, 3, 1},
+    {9, 1, 3},
+    {3, 9, 1},
+    {3, 1, 9},
+    {1, 9, 3},
+    {1, 3, 9}
+
+};
+struct A
+{
+    int a, b, c;
+};
+queue<A> q;
+int solve(int a, int b, int c)
+{
+    visited[a][b][c] = 1;
+    q.push({a, b, c});
+    while (q.size())
+    {
+        int a = q.front().a;
+        int b = q.front().b;
+        int c = q.front().c;
+        q.pop();
+        if (visited[0][0][0])
+            break;
+        for (int i = 0; i < 6; i++)
+        {
+            int na = max(0, a - _a[i][0]);
+            int nb = max(0, b - _a[i][1]);
+            int nc = max(0, c - _a[i][2]);
+            if (visited[na][nb][nc])
+                continue;
+            visited[na][nb][nc] = visited[a][b][c] + 1;
+            q.push({na, nb, nc});
+        }
+    }
+    return visited[0][0][0] - 1;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << solve(a[0], a[1], a[2]) << "\n";
+}
