96fa3b17926bfd772224037330f64911835fafd9
wldn143 <wldn143@gmail.com>
2022-10-31 22:55:10 +0900

    백준 9935 문자열 폭발

---
+++ 그리디,라인스위핑,투포인터/boj9935.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+string s, t, ret;
+int main()
+{
+    cin >> s >> t;
+    for (char a : s)
+    {
+        ret += a;
+        if (ret.size() >= t.size() && ret.substr(ret.size() - t.size(), t.size()) == t)
+        {
+            ret.erase(ret.end() - t.size(), ret.end());
+        }
+    }
+    if (!ret.size())
+        cout << "FRULA"
+             << "\n";
+    else
+        cout << ret << "\n";
+}
