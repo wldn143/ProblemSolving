ace8cf66e1f5dcf9bd17127834ac2e6aa1ace68b
wldn143 <wldn143@gmail.com>
2022-12-24 22:31:09 +0900

    백준 1535 안녕

---
+++ DP/boj1535.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int n, dp[101], cost[101], happy[101];
+double temp;
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &cost[i]);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &happy[i]);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 100; j > cost[i]; j--)
+        {
+            dp[j] = std::max(dp[j], dp[j - cost[i]] + happy[i]);
+        }
+    }
+    printf("%d\n", dp[100]);
+    return 0;
+}
