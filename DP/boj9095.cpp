0afae65d0a9c012b06df8aa85a39bd71db70e6ae
wldn143 <wldn143@gmail.com>
2022-12-31 23:23:12 +0900

    백준 9095 1,2,3 더하기

---
+++ DP/boj9095.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int t, n, dp[12];
+int main()
+{
+    cin >> t;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for (int i = 4; i <= 11; i++)
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+
+    while (t--)
+    {
+        cin >> n;
+        cout << dp[n] << "\n";
+    }
+}
