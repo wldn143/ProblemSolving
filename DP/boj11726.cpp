95953cbd14341ec19d925871d909ead50d356c05
wldn143 <wldn143@gmail.com>
2023-01-02 12:30:32 +0900

    백준 11726 2xn 타일링

---
+++ DP/boj11726.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, dp[1001];
+int main()
+{
+    cin >> n;
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++)
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    cout << dp[n] << "\n";
+}
