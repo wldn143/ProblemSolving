ebf311f246affe74d4c1fcd675c9a634e2931aeb
wldn143 <wldn143@gmail.com>
2023-01-11 12:54:07 +0900

    백준 1309 동물원

---
+++ DP/boj1309.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, dp[100001][3];
+
+int main()
+{
+    cin >> n;
+    dp[0][0] = 1;
+    dp[0][1] = 1;
+    dp[0][2] = 1;
+    for (int i = 1; i < n; i++)
+    {
+        dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % 9901;
+        dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+        dp[i][2] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+    }
+    cout << (dp[n - 1][0] + dp[n - 1][1] + dp[n - 1][2]) % 9901 << "\n";
+}
