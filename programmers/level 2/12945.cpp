d172d8d781c47bbc780985fca96a2a90dd450a23
wldn143 <wldn143@gmail.com>
2023-02-05 14:22:23 +0900

    프로그래머스 12945 피보나치 수

---
+++ programmers/level 2/12945.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+int dp[100001];
+int solution(int n)
+{
+    int answer = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    answer = dp[n];
+    return answer;
+}
