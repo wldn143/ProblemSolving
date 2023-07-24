8fd4dad37cfa53469e8a3c52d03fd4e6aa0a8e44
wldn143 <wldn143@gmail.com>
2023-07-24 22:55:25 +0900

    백준 2151 거울 설치

--- 백준/그래프이론, DFS, BFS/boj2151.cpp
+++ 백준/그래프이론, DFS, BFS/boj2151.cpp
@@ -1,99 +1,101 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
 #include <algorithm>
-#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#define INF 987654321
 using namespace std;
-typedef struct a
+int n, sy, sx, ey, ex, ret = INF;
+string map[51];
+vector<pair<int, int>> v;
+queue<pair<pair<int, int>, pair<int, int>>> q;
+int dir[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+int checked[51][51][4];
+int isValid(int y, int x)
 {
-    int y;
-    int x;
-} dir;
-
-int n;
-string arr[50];
-vector<dir> door;
-int visited[50][50][4];
-dir d[4] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-void bfs()
-{
-
-    queue<pair<pair<int, int>, int>> que; // 좌표 방향
-    for (int i = 0; i < 4; i++)
-    {
-        que.push(make_pair(make_pair(door[0].y, door[0].x), i));
-        visited[door[0].y][door[0].x][i] = 1;
-    }
-
-    while (!que.empty())
-    {
-        int y = que.front().first.first;
-        int x = que.front().first.second;
-        int dir = que.front().second;
-        que.pop();
-
-        int ny = y + d[dir].y;
-        int nx = x + d[dir].x;
-        if (ny < 0 || ny >= n || nx < 0 || nx >= n || arr[ny][nx] == '*')
-            continue;
-
-        if (arr[ny][nx] == '.' || arr[ny][nx] == '#')
-        {
-            if (visited[ny][nx][dir] > visited[y][x][dir] || visited[ny][nx][dir] == 0)
-            {
-                visited[ny][nx][dir] = visited[y][x][dir];
-                que.push(make_pair(make_pair(ny, nx), dir));
-            }
-        }
-        else if (arr[ny][nx] == '!')
-        {
-            if (visited[ny][nx][dir] > visited[y][x][dir])
-            { // 거울 설치 x
-                visited[ny][nx][dir] = visited[y][x][dir];
-                que.push(make_pair(make_pair(ny, nx), dir));
-            }
-            for (int i = 1; i <= 3; i += 2)
-            {
-                if (visited[ny][nx][(dir + i) % 4] > visited[y][x][dir] + 1)
-                { // 거울 설치 1
-                    visited[ny][nx][(dir + i) % 4] = visited[y][x][dir] + 1;
-                    que.push(make_pair(make_pair(ny, nx), (dir + i) % 4));
-                }
-            }
-        }
-    }
+    return (y >= 0 && x >= 0 && y < n && x < n && map[y][x] != '*');
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
     cin >> n;
     for (int i = 0; i < n; i++)
+        cin >> map[i];
+
+    for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
         for (int j = 0; j < n; j++)
         {
-            if (arr[i][j] == '#')
-                door.push_back({i, j});
-            for (int k = 0; k < 4; k++)
-                visited[i][j][k] = 2000000000;
+            if (map[i][j] == '#')
+                v.push_back({i, j});
         }
     }
 
-    bfs();
+    sy = v[0].first;
+    sx = v[0].second;
+    ey = v[1].first;
+    ex = v[1].second;
 
-    int result = 2000000000;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            for (int k = 0; k < 4; k++)
+                checked[i][j][k] = INF;
+
     for (int i = 0; i < 4; i++)
     {
-        if (visited[door[1].y][door[1].x][i] != 0)
-            result = min(result, visited[door[1].y][door[1].x][i]);
+        q.push({{sy, sx}, {i, 0}});
+        checked[sy][sx][i] = 0;
     }
-    cout << result - 1 << endl;
 
-    return 0;
+    while (!q.empty())
+    {
+        int y = q.front().first.first;
+        int x = q.front().first.second;
+        int d = q.front().second.first;
+        int cnt = q.front().second.second;
+        q.pop();
+
+        if (y == ey && x == ex)
+            ret = min(ret, cnt);
+
+        int ny = y + dir[d][0];
+        int nx = x + dir[d][1];
+
+        if (isValid(ny, nx))
+        {
+            if (checked[ny][nx][d] > cnt)
+            {
+                q.push({{ny, nx}, {d, cnt}});
+                checked[ny][nx][d] = cnt;
+            }
+
+            if (map[ny][nx] == '!')
+            {
+                if (d == 0 || d == 1)
+                {
+                    for (int j = 2; j <= 3; j++)
+                    {
+                        if (checked[ny][nx][j] > cnt + 1)
+                        {
+                            checked[ny][nx][j] = cnt + 1;
+                            q.push({{ny, nx}, {j, cnt + 1}});
+                        }
+                    }
+                }
+
+                else if (d == 2 || d == 3)
+                {
+                    for (int j = 0; j <= 1; j++)
+                    {
+                        if (checked[ny][nx][j] > cnt + 1)
+                        {
+                            checked[ny][nx][j] = cnt + 1;
+                            q.push({{ny, nx}, {j, cnt + 1}});
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << ret;
 }
